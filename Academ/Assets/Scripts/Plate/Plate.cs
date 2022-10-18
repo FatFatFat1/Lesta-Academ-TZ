e14f5bdcd8386e304cbabdfc5f46fa943af0196c
FatFatFat1 <48385210+FatFatFat1@users.noreply.github.com>
2022-10-18 17:22:56 +0700

    [add] Select Near

--- Academ/Assets/Scripts/Plate/Plate.cs
+++ Academ/Assets/Scripts/Plate/Plate.cs
@@ -10,13 +10,36 @@ public class Plate : MonoBehaviour
 
     [SerializeField] private Sprite _blockSprite;
     [SerializeField] private Sprite _freeSprite;
-    [SerializeField] private Sprite _filledSprite;
+    [SerializeField] private GameObject[] nearPlate = new GameObject[8];
 
 
     [SerializeField] private bool isSelected = false;
 
-    public bool IsSelected => isSelected;
 
+    public bool IsSelected => isSelected;
+    public GameObject[] NearPlate => nearPlate;
+
+    private void Start()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        FindNearPlate();
+        GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    void FindNearPlate()
+    {
+        Vector2 myPosition = transform.position;
+        Collider2D[] hits = Physics2D.OverlapCircleAll(myPosition, 2f);
+        int f = 0;
+        foreach (Collider2D i in hits)
+        {
+            if (i.gameObject != null)
+            {
+                nearPlate[f] = i.gameObject;
+                f++;
+            }
+        }
+    }
     public void UpdateState(string state)
     {
         switch (state)
@@ -31,7 +54,6 @@ public class Plate : MonoBehaviour
                 break;
             case GlobalVar.FILLEDSTATE:
                 _state = state;
-                GetComponent<SpriteRenderer>().sprite = _filledSprite;
                 break;
         }
     }
@@ -42,6 +64,7 @@ public class Plate : MonoBehaviour
     private void OnMouseDown()
     {
         GameObject oldPlate = null;
+        bool isNear = false;
         for (int i = 0; i < GlobalVar.AllPlate.Length; i++)
         {
             if (GlobalVar.AllPlate[i].GetComponent<Plate>().isSelected)
@@ -49,13 +72,25 @@ public class Plate : MonoBehaviour
                 oldPlate = GlobalVar.AllPlate[i];
             }
         }
-        if (oldPlate != null && oldPlate != gameObject)
+        if (oldPlate != null)
+        {
+            for (int i = 0; i < oldPlate.GetComponent<Plate>().NearPlate.Length; i++)
+            {
+                if (oldPlate.GetComponent<Plate>().NearPlate[i] == gameObject)
+                {
+                    isNear = true;
+                }
+            }
+        }
+
+        if (oldPlate != null && oldPlate != gameObject && isNear)
         {
             GlobalVar.GameController.GetComponent<SelectController>().Reselect(oldPlate, gameObject);
         }
-        else
+        else if (oldPlate == null || oldPlate == gameObject)
         {
             GlobalVar.GameController.GetComponent<SelectController>().Reselect(gameObject);
         }
+
     }
 }
